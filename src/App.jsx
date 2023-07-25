b75af79158ba9b928a7243f6fc4d907213014b34
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-07-25 20:34:52 -0300

    chore: modifica index e renomeia App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <header className="App-header">
+        <p>Landing Page</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
