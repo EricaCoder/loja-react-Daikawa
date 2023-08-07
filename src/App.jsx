fd40b9b8461041e1ed743335219295b8c7556849
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-08-07 13:13:17 -0300

    chore: ajusta chamada de ItemListContainer em App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import ItemCount from "./components/ItemCount";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
@@ -10,12 +9,7 @@ function App() {
       <header className="App-header">
         <p>Landing Page header</p>
       </header>
-      <ItemListContainer>
-        <ItemCount productName={"camiseta"} stock={5} initial={1} />
-        <ItemCount productName={"caneca"} stock={10} initial={1} />
-        <ItemCount productName={"garrafa"} stock={15} initial={1} />
-        <ItemCount productName={"moleskine"} stock={20} initial={1} />
-      </ItemListContainer>
+      <ItemListContainer />
     </div>
   );
 }
