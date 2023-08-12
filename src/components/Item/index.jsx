b45b2259cc1a8b2ea4e2e3b063080abaa028a7a5
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-08-12 09:39:43 -0300

    refactor: refatora Item

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,18 +1,19 @@
-export default function Item({
-  id,
-  title,
-  description,
-  price,
-  pictureUrl,
-  children,
-}) {
+import ItemCount from "../ItemCount";
+
+export default function Item({ item }) {
   return (
-    <li key={id} className="list-group-item col-sm m-3">
-      <img src={pictureUrl} alt={title} className="img-thumbnail h-auto  p-2" />
-      <h3 className="text-secondary">{title}</h3>
-      <p className="card-text">{description}</p>
-      <h2 className="card-subtilte mb-2 ">R$ {price}</h2>
-      <div>{children}</div>
+    <li key={item.id} className="list-group-item col-sm m-3">
+      <img
+        src={item.pictureUrl}
+        alt={item.title}
+        className="img-thumbnail h-auto  p-2"
+      />
+      <h3 className="text-secondary">{item.title}</h3>
+      <p className="card-text">{item.description}</p>
+      <h2 className="card-subtitle mb-2 ">R$ {item.price}</h2>
+      <div>
+        <ItemCount item={item} />
+      </div>
     </li>
   );
 }
