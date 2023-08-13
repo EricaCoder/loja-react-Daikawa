f9b6fdeadf84b417c9388679c3febaf5be1e8720
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-08-13 20:18:37 -0300

    chore: adiciona ItemDetailContainer para visualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
+import ItemDetailContainer from "./containers/ItemDetailContainer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <p>Landing Page header</p>
       </header>
+      <ItemDetailContainer />
       <ItemListContainer />
     </div>
   );
