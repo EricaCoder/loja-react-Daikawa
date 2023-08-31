932c75b9dc2794567853a9b427d490f85481b4aa
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-08-30 22:02:04 -0300

    feat: inicia o desafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import CartContext from "./context/cartContext.js";
+import CartProvider from "./providers/CartProvider.js";
 import { Router } from "./router/Router.jsx";
 
 function App() {
-  return <Router />;
+  return (
+    <CartContext.Provider>
+      <Router />
+    </CartContext.Provider>
+  );
 }
 
 export default App;
